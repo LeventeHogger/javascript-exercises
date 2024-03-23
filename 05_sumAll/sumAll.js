98a0c53b466d133c65dbf1cb14ea4716a325720f
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-23 21:37:05 +0100

    Simplified 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,14 +4,14 @@ const sumAll = function (from, to) {
 
   let sum = 0;
 
-  if (from <= to) {
-    for (let i = from; i <= to; i++)
-      sum += i;
+  if (from > to) {
+    let temp = from;
+    from = to;
+    to = temp;
   }
-  else {
-    for (let i = from; i >= to; i--)
-      sum += i;
-  }
+
+  for (let i = from; i <= to; i++)
+    sum += i;
 
   return sum;
 };
