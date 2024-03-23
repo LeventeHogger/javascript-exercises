5d38b79d12683a196929f13f76649b4e38fa3578
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-23 21:29:27 +0100

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (from, to) {
+  if (typeof from != "number" || typeof to != "number" || from < 0 || to < 0)
+    return "ERROR";
 
+  let sum = 0;
+
+  if (from <= to) {
+    for (let i = from; i <= to; i++)
+      sum += i;
+  }
+  else {
+    for (let i = from; i >= to; i--)
+      sum += i;
+  }
+
+  return sum;
 };
 
+console.log(sumAll(1, 69))
 // Do not edit below this line
 module.exports = sumAll;
