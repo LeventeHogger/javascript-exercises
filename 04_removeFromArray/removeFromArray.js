dcc0b4a29861151a013216b2f87492ea64cf47fa
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-23 21:04:18 +0100

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (targetArray, ...itemsToRemove) {
+  let outputArray = [];
 
+  for (let i = 0; i < targetArray.length; i++) {
+    if (!itemsToRemove.includes(targetArray[i]))
+      outputArray.push(targetArray[i]);
+  }
+
+  return outputArray;
 };
+
+console.log(removeFromArray([1, 2, 3], 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
