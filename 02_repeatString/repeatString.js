0e9fb1f9f48db5d0a4c8c720d0ec11ba8cdf293c
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-23 20:06:27 +0100

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  let currentString = '';
+  for (let i = 0; i < num; i++) {
+    currentString += string;
+  }
+  return currentString;
 };
 
+console.log(repeatString('hey', 3));
 // Do not edit below this line
 module.exports = repeatString;
