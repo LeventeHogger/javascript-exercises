071c0054f296f1b63aa3eb5f17218f44967a43d7
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-23 20:17:09 +0100

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let splitString = string.split('');
+  let outputString = '';
 
+  for (let i = splitString.length - 1; i >= 0; i--) {
+    outputString += splitString[i];
+  }
+
+  return outputString;
 };
+
+console.log(reverseString('megfordítja azt amit ide írok'));
 
 // Do not edit below this line
 module.exports = reverseString;
