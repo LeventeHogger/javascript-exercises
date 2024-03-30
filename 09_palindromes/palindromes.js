863c7e4ea7b6df5fc745b67f554037c71d03d4cc
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-30 08:24:09 +0100

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (words) {
+  const allowed = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz0123456789";
 
+  const wordsfiltered = words
+    .toLowerCase()
+    .split("")
+    .filter((char) => allowed.includes(char))
+    .join("");
+
+  const reversed = wordsfiltered.split("").reverse().join("");
+
+  return reversed == wordsfiltered;
 };
 
 // Do not edit below this line
