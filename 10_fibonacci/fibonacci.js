8aa7f3e5c3b9514e8ccf1ec9fe9d8ab81abbbbf3
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-30 10:32:08 +0100

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (which) {
+  if (typeof which != "number")
+    parseInt(which);
 
+  if (which < 0)
+    return "OOPS";
+
+  const sequence = [0, 1];
+  for (let i = 2; i <= parseInt(which); i++) {
+    sequence[i] = sequence[i - 2] + sequence[i - 1];
+  }
+
+  return sequence[which];
 };
 
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
