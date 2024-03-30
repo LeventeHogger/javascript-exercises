f2f22b4c6e4ce724d272d82432d5abf497611840
LeventeHogger <42783111+LeventeHogger@users.noreply.github.com>
2024-03-30 12:08:54 +0100

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  const sorted = people.sort((a, b) => {
+    if (!a.yearOfDeath)
+      a.yearOfDeath = currentYear;
+    if (!b.yearOfDeath)
+      b.yearOfDeath = currentYear;
+
+    return a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1
+  });
+
+  return sorted[0];
 };
 
 // Do not edit below this line
